1fac9e482a09d19f15916a17d6bcb03a854d78de
Giordanov2 <110947927+Yoryov@users.noreply.github.com>
2022-10-17 00:47:50 -0500

    Update

--- Basics/trycatch.ps1
+++ Basics/trycatch.ps1
@@ -1,5 +1,18 @@
 $folderPath = '.\intel'
 
+# Handling Nonterminanting errors
+# With a "Stop" Error Action
+
+$files2 = Get-ChildItem -Path $folderPath -ErrorAction Stop
+Write-Host "This shouldn't run"
+$files2.foreach({
+    $fileText = Get-Content $files2
+    $fileText[0]
+})
+
+
+# Handling Terminating errors
+# With the ""try/catch/finally" syntax
 try {
     # initial code
     $files = Get-ChildItem -Path $folderPath -ErrorAction Stop
