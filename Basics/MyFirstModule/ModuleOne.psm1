e6e6b9975c49a9a3ba24500a5c3378925032e0bc
Giordanov2 <110947927+Yoryov@users.noreply.github.com>
2022-11-12 18:47:54 -0500

    Update

---
+++ Basics/MyFirstModule/ModuleOne.psm1
@@ -0,0 +1,15 @@
+# Quick tip:
+#   To check the "approved" verbs
+#   Get-Verb 
+
+function Install-Malware {
+    param ()
+}
+
+function Remove-Malware {
+    param ()
+}
+
+function Hide-Malware {
+    param ()
+}
