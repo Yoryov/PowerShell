fb42025292a61aac152be9b55865e486d80137ca
Giordanov2 <110947927+Yoryov@users.noreply.github.com>
2022-11-15 20:03:30 -0500

    Update how to create a module with ps

--- Basics/modules.ps1
+++ Basics/modules.ps1
@@ -1,9 +1,9 @@
-# To create your own module we need to create 3 things first:
-# - Folder (the module container) and needs to be in one of this 3 locations
-#   - System
-#   - All Users
-#   - Current User
-# - The Module File (.psm1)
-# - The Module Manifest (.psd1)
+<#
+To create your own module we need to create 3 things first:
+- Folder (the module container)
+- The Module File (.psm1)
+- The Module Manifest (.psd1)
+#> 
+
 
 
